f36f8ac90b0b683ef273be0d5d118daae97052cd
lucaschm <luca.schmitt@haw-hamburg.de>
2025-04-16 17:20:43 +0200

    add obsidian monitoring to exit_handler

--- src/autocommit/exit_handler.py
+++ src/autocommit/exit_handler.py
@@ -1,6 +1,8 @@
 import signal
 import sys
 from watchdog.observers import Observer
+import psutil
+import threading
 
 from autocommit.logger import get_logger
 from autocommit.git import commit_and_push
@@ -15,6 +17,7 @@ class ExitHandler:
         self._workspace = workspace
         self._observer = observer
         self.prepare_for_exit()
+        self.monitor_obsidian_exit(self.on_obsidian_exit)
 
     def handle_exit(self, *args):
         logger.info("Exiting... Committing last edited files.")
@@ -26,3 +29,23 @@ class ExitHandler:
         signal.signal(signal.SIGINT, self.handle_exit)
         signal.signal(signal.SIGTERM, self.handle_exit)
 
+    def on_obsidian_exit(self):
+        logger.info("Obsidian has exited! Committing last edited files.")
+        commit_and_push(self._workspace, '*', "save * (autocommit exit)")
+
+    def monitor_obsidian_exit(self, callback):
+        # Find the first Obsidian process
+        for proc in psutil.process_iter(['pid', 'name']):
+            if 'obsidian' in proc.info['name'].lower():
+                threading.Thread(target=self.wait_and_trigger, args=(proc.pid, callback), daemon=True).start()
+                logger.info(f"Monitoring Obsidian (PID: {proc.pid}) for exit.")
+                return
+        logger.warning("Obsidian is not currently running.")
+
+    def wait_and_trigger(self, pid, callback):
+        try:
+            proc = psutil.Process(pid)
+            proc.wait()  # This blocks until the process exits
+            callback()
+        except psutil.NoSuchProcess:
+            logger.info("Obsidian process already exited.")
